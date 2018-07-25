ccd7812ad42531de283e50b949d3d7ecbb24e606
Dan Njoku <dannjoku1@gmail.com>
2018-07-24 20:34:49 -0400

    Determining database input

--- chatbot_database.py
+++ chatbot_database.py
@@ -17,6 +17,29 @@ def format_data(data):
     data = data.replace("\n"," newlinechar ").replace("\r"," newlinechar ").replace('"',"'")
     return data
 
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print("find_parent", e)
+        return False
+
+# ensure that data is 'acceptable' and meets minimum requirements
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1: #between 2 - 50 words
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data = '[deleted]' or data = '[removed]': # deleted/removed comments
+        return False
+    else:
+        return True
+
 def find_parent(pid):
     try:
         sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
@@ -26,7 +49,7 @@ def find_parent(pid):
             return result[0]
         else: return False
     except Exception as e:
-        print("find_parent", e)
+        #print("find_parent", e)
         return False
 
 # main loop
@@ -35,8 +58,9 @@ if __name__ == "__main__":
     row_counter = 0
     paired_rows = 0
 
-    with open("Daniels-MBP:/Users/dan/documents/RC_{}".format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+    with open("/Users/dan/Documents/RC_{}".format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
         for row in f:
+            #print(row)
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
@@ -44,5 +68,11 @@ if __name__ == "__main__":
             created_utc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
-
             parent_data = find_parent(parent_id)
+
+            # if more than one person upvoted the post
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id) #is there a reply that has a score greater than current score
+                if existing_comment_score:
+                    if score > existing_comment_score:
+
